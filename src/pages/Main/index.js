c676e792169b7d7858a873cf7df7e058c32a3488
Hugo Frota <herbtyh@gmail.com>
2023-05-17 14:53:34 -0300

    Estilizar página main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { FaGithub, FaPlus } from 'react-icons/fa';
 
-import { Title } from './styles';
+import { Container, Form, SubmitButton } from './styles';
 
 const Main = () => {
-  return <Title>Main</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithub size={25} />
+        Meus Repositorios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositorios" />
+
+        <SubmitButton>
+          <FaPlus color="#fff" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 };
 
 export default Main;
