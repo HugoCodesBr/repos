8b946ae8a4da30f18dc06f6c541105844686de8c
Hugo Frota <herbtyh@gmail.com>
2023-05-17 14:23:21 -0300

    first commit

---
+++ src/Routes/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import Main from '../pages/Main';
+import Repositorio from '../pages/Repositorio';
+
+const RoutesApp = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/repositorio/:repositorio" element={<Repositorio />} />
+    </Routes>
+  );
+};
+
+export default RoutesApp;
