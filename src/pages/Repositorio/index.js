247cb39c05173702397562dfdf1371a3e05bdb18
Hugo Frota <herbtyh@gmail.com>
2023-05-19 14:55:22 -0300

    Consumir detalhes do respositorio da api

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { Container } from './styles';
+
+import api from '../../services/api';
 
 const Repositorio = () => {
   const { repositorio } = useParams();
+  const [meuRepositorio, setMeuRepositorio] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  return <h1 style={{ color: '#fff' }}>{repositorio}</h1>;
+  useEffect(() => {
+    async function load() {
+      const nomeRepo = repositorio;
+
+      const [repositorioData, issuesData] = await Promise.all([
+        api.get(`/repos/${nomeRepo}`),
+        api.get(`/repos/${nomeRepo}/issues`, {
+          params: {
+            state: 'open',
+            per_page: 5,
+          },
+        }),
+      ]);
+
+      setMeuRepositorio(repositorioData.data);
+      setIssues(issuesData.data);
+      setLoading(false);
+    }
+
+    load();
+  }, [repositorio]);
+
+  return <Container></Container>;
 };
 
 export default Repositorio;
